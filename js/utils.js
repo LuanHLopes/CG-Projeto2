271a074f9d79cd8adb45bdea958052a0a5b476e6
Luan Henrique <luhenry.lopes@gmail.com>
2025-11-21 20:23:19 -0300

    Update menu e botões

--- js/utils.js
+++ js/utils.js
@@ -1,2 +1,21 @@
-// Bloqueia o menu botão direito para não atrapalhar o gameplay
 window.addEventListener("contextmenu", (e) => e.preventDefault(), false);
+
+window.addEventListener("keydown", (e) => {
+  if (e.code === "F11") {
+    e.preventDefault();
+
+    if (!document.fullscreenElement) {
+      document.body.requestFullscreen().catch((err) => {
+        console.warn("Erro ao tentar tela cheia:", err);
+      });
+    } else {
+      document.exitFullscreen();
+    }
+  }
+
+  if (e.code === "Escape") {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    }
+  }
+});
