81ac2a2cf27f70b9ecc0cdf13bf83dab56e9698c
Luan Henrique <luhenry.lopes@gmail.com>
2025-11-21 21:36:38 -0300

    Update correção de erros

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ import "./utils.js";
 import "./scripts/fisica.js";
 import "./scripts/movimento.js";
 import "./scripts/placar.js";
-import "./scripts/arremesso.js";
 import "./scripts/trajetoria.js";
+import "./scripts/arremesso.js";
 import "./scripts/loader.js";
 
 console.log("Todos os scripts do jogo foram carregados!");
